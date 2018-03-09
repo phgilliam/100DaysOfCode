ecf81240575912031ab831b915fb232ae629a628
Paul <paulhgilliam@gmail.com>
2018-03-09 11:41:48 -0600

    ex13.py

--- PracticePython/ex13.py
+++ PracticePython/ex13.py
@@ -1,4 +1,5 @@
-fib=[]
-fib = list(map(lambda x,y: x+y in range(5),fib))
+x = int(input('Length of list of Fibonacci numbers: '))
+
+fib = [int((((1+5**0.5)/2)**n-((1-5**0.5)/2)**n)/5**0.5) for n in range(x)]
 
 print(fib)
